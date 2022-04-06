10b3b0bb7f3ecbdecdd558b5455f91c471ac7e05
Temirlan006 <ahunzanovtemirlan@gmail.com>
2022-04-06 21:17:24 +0600

    dz1

---
+++ lesson1/main.js
@@ -0,0 +1,53 @@
+const a = "a"; //string
+const b = 20; // number
+let c = "hello! " // string
+const bool = true; // boolean
+const bool2 = false;
+
+// console.log(typeof bool)
+// console.log(c)
+//
+// c = 40
+//
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+const obj = {
+    key: "value",
+    username: "Sara",
+    data: {
+        first_name: "Jack",
+        last_name: "Rus"
+    }
+}
+
+// console.log(obj)
+// console.log(obj.username)
+// console.log(obj.key)
+// console.log(obj.data.first_name)
+// console.log(obj.data.last_name)
+
+const arr = ["Aidana", true, false, 34]
+// console.log(arr[0], arr[1])
+//
+// console.log(typeof arr)
+// console.log(arr)
+
+
+// const name = prompt("Как вас зовут?")
+// console.log("Hello! " + name)
+
+
+// const first_number = prompt("первое число?")
+// const last_number = prompt("второе число?")
+//
+// if(first_number > last_number){
+//     console.log(first_number + ">" + last_number)
+// } else if(first_number < last_number){
+//     console.log(last_number + ">" + first_number)
+// } else {
+//     console.log(first_number + "=" + last_number)
+// }
+
+const otvet = alert("за домашку 10 баллов")
