4791014b1a3e90ecc165a1732c808d1e76e72279
Temirlan006 <ahunzanovtemirlan@gmail.com>
2022-04-23 21:36:57 +0600

    lesson4

---
+++ lesson4/main.js
@@ -0,0 +1,43 @@
+// console.log(document)
+//
+const blocks = document.getElementsByClassName('block');
+// console.log(blocks)
+
+const block3 = document.getElementById('block3')
+// console.log(block3.classList)
+//
+// block3.className = "changedClass"
+// console.log(block3.classList)
+//
+// block3.classList.add("changedList")
+// block3.classList.remove("newClass")
+// console.log(block3.classList)
+//
+// block3.onclick = () => {
+//     block3.classList.toggle("active")
+// }
+//
+
+// const block = document.querySelector('.block');
+//
+// const blocks1 = document.querySelectorAll('.block')
+// console.log(blocks1)
+
+const button = document.getElementById('button');
+const input = document.getElementById('input');
+
+button.onclick = () => {
+    const div = document.createElement('div');
+    const deleteButton = document.createElement('button');
+    deleteButton.innerText = "delete";
+    deleteButton.onclick = () => {
+        div.remove()
+    }
+
+    
+    div.setAttribute('class', 'block');
+    div.innerText = input.value;
+    div.append(deleteButton);
+    document.body.append(div)
+    input.value = "";
+}
